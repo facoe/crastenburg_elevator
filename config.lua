b9780d753ff94a54fd84f2d09f69a47c51862c04
facoe <55077903+facoe@users.noreply.github.com>
2025-03-26 17:50:22 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,53 +1,20 @@
 Config = {}
 
-Config.UseESX = false						-- Use ESX Framework
-Config.UseQBCore = true					-- Use QBCore Framework (Ignored if Config.UseESX = true)
+Config.UseESX = false						
+Config.UseQBCore = true					
 
-Config.UseThirdEye = true				-- If true uses third eye.
-Config.ThirdEyeName = 'qb-target' 			-- Name of third eye aplication
-Config.Use3DText = false                        -- Use 3D text to interact
-Config.NHMenu = false						-- Use NH-Context [https://github.com/whooith/nh-context]
-Config.QBMenu = true						-- Use QB-Menu (Ignored if Config.NHInput = true) [https://github.com/qbcore-framework/qb-menu]
-Config.OXLib = false						-- Use the OX_lib (Ignored if Config.NHInput or Config.QBInput = true) [https://github.com/overextended/ox_lib] !! must add shared_script '@ox_lib/init.lua' and lua54 'yes' to fxmanifest!!
-Config.ElevatorWaitTime = 1					-- How many seconds until the player arrives at their floor
+Config.UseThirdEye = true				
+Config.ThirdEyeName = 'qb-target' 			
+Config.Use3DText = false                       
+Config.NHMenu = false						
+Config.QBMenu = true						
+Config.OXLib = false						
+Config.ElevatorWaitTime = 1					
 
 Config.Notify = {
-	enabled = false,							-- Display hint notification?
-	distance = 3.0,							-- Distance from elevator that the hint will show
-	message = "Target the elevator to use"	-- Text of the hint notification
-}
-
---[[
-	USAGE
-	To add an elevator, copy the table below and configure as needed:
-		coords = vector3 coords of center of elevator
-		heading = Direction facing out of the elevator
-		level = What floor are they going to
-		label = What is on that floor
-		jobs = OPTIONAL: Table of job keys that are allowed to access that floor and value of minimum grade of each job
-		items = Opcional: cualquier elemento que se requiere para acceder a ese piso (solo requiere uno de los elementos enumerados)
-		jobAndItem = OPTIONAL: If true, you must you have a required job AND a required items. If false or nil no items are needed
-	
-]]
-
---[[
-	ExampleElevator = {	
-		{
-			coords = vector3(xxx, yyy, zzz), heading = 0.0, level = "Floor 2", label = "Roof",
-			jobs = { ["police"] = 0, ["ambulance"] = 0, ["casino"] = 0 },
-			items = { "casino_pass_bronze", "casino_pass_silver", "casino_pass_gold" }
-		},
-		{
-			coords = vector3(xxx, yyy, zzz), heading = 0.0, level = "Floor 1", label = "Penthouse",
-			jobs = { ["police"] = 0, ["ambulance"] = 0, ["casino"] = 0 },
-			items = { "casino_pass_gold" },
-			jobAndItem = true
-		},
-		{
-			coords = vector3(xxx, yyy, zzz), heading = 0.0, level = "Floor 0", label = "Ground"
-		},
-	},
-]]
+	enabled = false,							
+	distance = 3.0,							
+	message = "Target the elevator to use"	
 
 Config.Elevators = {
 	
